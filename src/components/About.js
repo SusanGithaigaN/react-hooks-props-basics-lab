ed53720edffe03f32a7131f6752474f592a69773
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-01-10 15:40:37 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,14 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
+  const myParagraph = props.bio;
   return (
     <div id="about">
       <h2>About Me</h2>
       {/* render a <p> element if the bio is an empty string */}
-      <p>{props.bio}</p>
+      {
+        myParagraph && <p>{props.bio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.links.github} linkedin={props.links.linkedin}/>
       
